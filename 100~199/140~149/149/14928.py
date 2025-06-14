6a23b361d42c09d7b08a5ce605863c85e7cbc7ca
Kim Jinsoo <kjs.fficial@gmail.com>
2025-06-14 20:24:51 +0900

    20250614

---
+++ 100~199/140~149/149/14928.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    N = int(input())
+    print(N % 20000303)
+
+if __name__ == "__main__":
+    solve()
---
+++ 200~299/260~269/260/26082.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define FASTIO cin.tie(nullptr); ios::sync_with_stdio(false);
+using ll = long long;
+using namespace std;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int A, B, C; cin >> A >> B >> C;
+    int D = 3 * B * C / A;
+    cout << D << endl;
+}
+
+int main() {
+    FASTIO
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+}
